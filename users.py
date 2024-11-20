48a5b5f6ef71eaf93ec4898930b16967d8a523b4
Ubuntu <ubuntu@vps-9ef52d87.vps.ovh.net>
2024-11-20 18:39:42 +0000

    correctif standard pep8

--- users.py
+++ users.py
@@ -1,56 +1,73 @@
 import streamlit as st
 import random
+
+
 class MembresInscription:
     def __init__(self, name: str, mail: str):
         self.name = name  # Utilise les valeurs passées en argument
         self.mail = mail
+
     def get_name(self):
         return self.name
+
     def get_mail(self):
         return self.mail
+
     def generate_card_id(self):
         unique_id = ""
         len_name = len(self.name)
         len_mail = len(self.mail)
         for i in range(len_name * len_mail):
-            id_choice_phase = random.choice([self.name,self.mail])
+            id_choice_phase = random.choice([self.name, self.mail])
             unique_id += str(ord(random.choice(id_choice_phase)))
         print(unique_id)
         return int(unique_id)
+
+
 class CarteMembre:
-    def __init__(self,unique_id):
+    def __init__(self, unique_id):
         self.unique_id = unique_id
+
     def get_unique_id(self):
         return self.unique_id
+
 
 def input_users():
     # Crée un formulaire avec un bouton de soumission
     with st.form("add_question_form", clear_on_submit=True):
-        name = st.text_input(label="Name", placeholder="Jean Dupont", key = "name_user")
-        mail = st.text_input(label="Mail", placeholder="toto@gmail.com")  # Retirer type="email"
-        submit = st.form_submit_button("Submit")  # Bouton de soumission
+        name = st.text_input(label="Name",
+                             placeholder="Jean Dupont", key="name_user")
+        mail = st.text_input(label="Mail",
+                             placeholder="toto@gmail.com")
+        submit = st.form_submit_button("Submit")
         if submit:
             # Crée une instance avec les valeurs saisies
             membre = MembresInscription(name, mail)
             # Stocke dans la session pour réutilisation
             st.session_state.membre_inscription = membre
-            st.write(f"Membre ajouté : {membre.get_name()} ({membre.get_mail()})")  # Affiche le membre ajouté
+            st.write(f"\
+                Membre ajouté : {membre.get_name()} \
+                    ({membre.get_mail()})")  # Affiche le membre ajouté
+
+
 def create_user():
     if "membre_inscription" not in st.session_state:
         st.session_state.membre_inscription = None
-    
     input_users()
     if st.session_state.membre_inscription is not None:
         if "carte_membre" not in st.session_state:
             st.session_state.carte_membre = None
         unique_id = st.session_state.membre_inscription.generate_card_id()
         st.session_state.carte_membre = CarteMembre(unique_id)
-        if type(st.session_state.carte_membre) == CarteMembre:
+        if type(st.session_state.carte_membre) is CarteMembre:
             st.write(f"{st.session_state.carte_membre.get_unique_id()=}")
+
+
 def connect_user():
-    input_card  = st.text_input(label="IdUser:")
+    input_card = st.text_input(label="IdUser:")
+    session = st.session_state
     if st.button("Connection"):
-        if int(input_card) == st.session_state.carte_membre.get_unique_id():
+        if int(input_card) == session.carte_membre.get_unique_id():
             st.write("Connection Success")
             st.session_state.state_connection = True
             st.rerun()
@@ -59,24 +76,29 @@ def connect_user():
             st.write(f"{int(input_card)=}")
             st.write("Connection Failed")
             st.session_state.state_connection = False
+
+
 def disconnect_user():
     if st.button("Déconnexion"):
         st.session_state.state_connection = False
         st.rerun()
+
+
 def start_member():
-        if "state_connection" not in st.session_state:
-            st.session_state.state_connection = False
-        st.title("Users:")
-        if "carte_membre" in st.session_state and "membre_inscription" in st.session_state:
-            
-            if st.session_state.state_connection == True:
-                st.write("Je suis en état connecté")
-                disconnect_user()
-            else:
-                connect_user()
-                st.write("Je ne suis pas en état connecté")
+    session = st.session_state
+    if "state_connection" not in session:
+        st.session_state.state_connection = False
+    st.title("Users:")
+    if "carte_membre" in session and "membre_inscription" in session:
+        if session.state_connection is True:
+            st.write("Je suis en état connecté")
+            disconnect_user()
         else:
-            create_user()
+            connect_user()
+            st.write("Je ne suis pas en état connecté")
+    else:
+        create_user()
+
 
 if __name__ == "__main__":
     start_member()
