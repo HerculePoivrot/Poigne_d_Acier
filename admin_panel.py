a217db1c972aeb7cc8eda2686263f7fd5c4406d6
Ubuntu <ubuntu@vps-9ef52d87.vps.ovh.net>
2024-11-20 18:09:29 +0000

    patch admin interface

--- admin_panel.py
+++ admin_panel.py
@@ -1,6 +1,7 @@
 from admin_session import ADMIN_ID, ADMIN_PASSWORD
 import streamlit as st
 
+
 def connection_admin():
     """Fonction pour gérer la connexion administrateur."""
     if "admin_authentified" not in st.session_state:
@@ -11,8 +12,13 @@ def connection_admin():
         return  # Évite de recréer les champs si déjà authentifié
 
     # Champs de connexion avec des clés dynamiques uniques
-    name = st.text_input(label="Name", key="name_admin_connection")
-    password = st.text_input(label="Password", type="password", key="password_admin_connection")
+    name = st.text_input(
+        label="Name",
+        key=f"name_admin_connection_{st.session_state.focus_interface}")
+    password = st.text_input(
+        label="Password",
+        type="password",
+        key=f"password_admin_connection_{st.session_state.focus_interface}")
 
     # Bouton de connexion
     if st.button("Connection", key="connection_button"):
@@ -21,6 +27,7 @@ def connection_admin():
             st.rerun()  # Recharge pour afficher le panneau admin
         else:
             st.error("Échec de la connexion. Vérifiez vos identifiants.")
+
 
 def admin_pannel():
     """Panneau administrateur principal."""
@@ -32,6 +39,8 @@ def admin_pannel():
     else:
         # Affiche le panneau admin après authentification
         st.success("Bienvenue dans le panneau administrateur.")
-        if st.button("Deconnexion", key="deconnexion_button"):
+        if st.button(label="Deconnexion",
+                     key=f"\
+                     deconnexion_button_{st.session_state.focus_interface}"):
             st.session_state.admin_authentified = False
             st.rerun()  # Recharge pour retourner au formulaire de connexion
--- interface.py
+++ interface.py
@@ -1,33 +1,48 @@
 import streamlit as st
 
 from users import start_member
-from admin_panel import connection_admin, admin_pannel
-def switch_interface(interface:str):
+from admin_panel import admin_pannel
+
+
+def switch_interface(interface: str):
     st.session_state.focus_interface = interface
     st.rerun()
+
+
 def switch_member():
     switch_interface("member")
+
+
 def switch_admin():
     switch_interface("admin")
+
+
 def switch_acceuil():
     switch_interface("default")
+
+
 def switch_connection():
     switch_interface("connect_member")
+
+
 def switch_register():
     switch_interface("register_member")
+
 
 def interface_admin():
     if st.button("Acceuil"):
         switch_acceuil()
-    connection_admin()
     admin_pannel()
-    
+
+
 def interface_register():
     start_member()
     if st.button("Souhaitez vous plutôt vous connecter ?"):
         switch_connection()
     if st.button("Acceuil"):
         switch_acceuil()
+
+
 def interface_member():
     if st.button("Je m'inscrit"):
         switch_register()
@@ -35,17 +50,23 @@ def interface_member():
         switch_connection()
     elif st.button("Acceuil"):
         switch_acceuil()
+
+
 def interface_connection():
     start_member()
     if st.button("Pas encore inscrit ? Inscrivez vous!"):
         switch_register()
     if st.button("Acceuil"):
         switch_acceuil()
+
+
 def interface_acceuil():
     if st.button("Espace Membre"):
         switch_member()
     elif st.button("Espace Admin"):
         switch_admin()
+
+
 def interface():
     st.write("Bienvenue à la Poigne d'acier")
     if "focus_interface" not in st.session_state:
@@ -60,6 +81,7 @@ def interface():
         interface_connection()
     elif st.session_state.focus_interface == "admin":
         interface_admin()
+
 
 if __name__ == "__main__":
     interface()
