f303e25bf2afbebc32dd8f3b113f1ec070e10845
Astambar <leuchartjason@gmail.com>
2024-11-21 13:22:45 +0100

    add_instance

--- utils.py
+++ utils.py
@@ -1,8 +1,13 @@
-from sqlmodel import Session
+from sqlmodel import Session, select
 from init_db import engine
 def add_instance(instance_object:object):
     with Session(engine) as sessionsql:
         sessionsql.add(instance_object)
         sessionsql.commit()
 
-    
+def update_instance(instance_object:object, focus, new_value):
+    with Session(engine) as sessionsql:
+        statement = select(instance_object).where(instance_object.focus == new_value)
+        results = sessionsql.exec(statement)
+        my_object = results.one()
+        print(f"{type(my_object)=} : {my_object}")
